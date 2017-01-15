b8789838390d41a73ace33877fc00bc90977710e
Michael Abadi Santoso <abadi_kaka@yahoo.com>
2017-01-16 01:56:52 +0700

    FD-3112-broken

--- ios/RCTOkkamiSdkImplementation/RCTOkkamiSdkImplementation/RCTOkkamiMain.swift
+++ ios/RCTOkkamiSdkImplementation/RCTOkkamiSdkImplementation/RCTOkkamiMain.swift
@@ -6,11 +6,58 @@
 //  Copyright © 2016 michaelabadi.com. All rights reserved.
 //
 import UIKit
+import Foundation
+import Moya
+import Moya_ModelMapper
+import Mapper
+import RxSwift
+import RxCocoa
 
+struct Repository: Mappable {
+    
+    let identifier: Int
+    let language: String
+    let name: String
+    let fullName: String
+    
+    init(map: Mapper) throws {
+        try identifier = map.from("id")
+        try language = map.from("language")
+        try name = map.from("name")
+        try fullName = map.from("full_name")
+    }
+}
+
+struct Issue: Mappable {
+    
+    let identifier: Int
+    let number: Int
+    let title: String
+    let body: String
+    
+    init(map: Mapper) throws {
+        try identifier = map.from("id")
+        try number = map.from("number")
+        try title = map.from("title")
+        try body = map.from("body")
+    }
+}
 @objc public class RCTOkkamiMain: NSObject {
     
-    //var bridge: RCTBridge!
-    //var bridge: RCTBridge!
+    let disposeBag = DisposeBag()
+    let items =  Variable([String]())
+    var data = [String]()
+    var provider: RxMoyaProvider<GitHub>!
+    var issueTrackerModel: IssueTrackerModel!
+    
+    /*var latestRepositoryName: Observable<String> {
+        return
+            searchBar
+            .rx_text
+            .throttle(0.5, scheduler: MainScheduler.instance)
+            .distinctUntilChanged()
+    }*/
+    
     
     public class func newInstance() -> RCTOkkamiMain {
         return RCTOkkamiMain()
@@ -18,6 +65,27 @@ import UIKit
     
     public func hello() {
         print("hello world")
+    }
+    
+    public func setupRx() {
+        // First part of the puzzle, create our Provider
+        provider = RxMoyaProvider<GitHub>()
+        
+        // Now we will setup our model
+        //issueTrackerModel = IssueTrackerModel(provider: provider, repositoryName: latestRepositoryName)
+        
+        provider.request(.Calendar).subscribe { event in
+            switch event {
+            case let .next(response):
+//                data.append(response.data)
+                print(response.data)
+            case let .error(error):
+                print(error)
+            default:
+                break
+            }
+        }
+        
     }
     
     /*public func testEvent( eventName: String ) {
